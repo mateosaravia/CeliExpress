55e840a261134a7856841e7bcbcb37d1878d39cd
mateosaravia <msaravia2710@gmail.com>
2023-08-23 21:04:14 -0300

    Changes in user model

--- application/src/models/users/user_model.py
+++ application/src/models/users/user_model.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 
 def get_current_datetime():
     return datetime.now()
 
 class UserSchema(BaseModel):
-    name: str
-    email: str
-    password: str
+    name: str = Field(min_length=3, max_length=50)
+    email: EmailStr = Field(default=None, min_length=0)
+    password: str = Field(default=None, min_length=0)
     age: int = Field(ge=12)
     created_at: datetime = Field(default_factory=get_current_datetime)
